64768658277f08636b270066049ba4752d17b929
949027 <82878668+949027@users.noreply.github.com>
2022-04-27 16:30:07 +0700

    Added ingredient models

--- bakecakeapp/admin.py
+++ bakecakeapp/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from bakecakeapp.models import Level, Form, Berry, Decor, Topping
+from manager.models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Level)
+class LevelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Topping)
+class ToppingAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Decor)
+class DecorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Berry)
+class BerryAdmin(admin.ModelAdmin):
+    pass
--- bakecakeapp/models.py
+++ bakecakeapp/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+
+class Level(models.Model):
+    amount = models.IntegerField(default=1, verbose_name="Количество уровней")
+    price = models.IntegerField("Цена")
+
+    def __str__(self):
+        return str(self.amount)
+
+    class Meta:
+        verbose_name = "Количество уровней"
+        verbose_name_plural = "Количество уровней"
+
+
+class Form(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование")
+    price = models.IntegerField("Цена")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Форма"
+        verbose_name_plural = "Формы"
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование")
+    price = models.IntegerField("Цена")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Топпинг"
+        verbose_name_plural = "Топпинг"
+
+
+class Berry(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование")
+    price = models.IntegerField("Цена")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Ягода"
+        verbose_name_plural = "Ягоды"
+
+
+class Decor(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование")
+    price = models.IntegerField("Цена")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Декор"
+        verbose_name_plural = "Декор"
