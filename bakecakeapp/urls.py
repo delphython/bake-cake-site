ca85bc4ffc34969625866ae332b21f019dab311f
delphython <delphython@gmail.com>
2022-04-27 16:01:31 +0300

    Add static url

--- bakecakeapp/urls.py
+++ bakecakeapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [
-    path('', views.index),
-    path('lk/', views.lk)
-]
+urlpatterns = [path("", views.index), path("lk/", views.lk)] + static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT
+)
