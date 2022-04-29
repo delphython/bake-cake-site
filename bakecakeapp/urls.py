1b415027a3affd4824d0cf1c2187a79fdb669111
delphython <delphython@gmail.com>
2022-04-29 16:29:37 +0300

    Add payment.urls

--- bakecakeapp/urls.py
+++ bakecakeapp/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import include, path
 
 from django.conf import settings
 from . import views
 
 
 urlpatterns = [
-    path("", views.index, name='index'),
+    path("", views.index, name="index"),
     path("lk/", views.lk),
     path("order/", views.register_order),
+    path("payment/", include("payment.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
