e83a2bc851e47b7e1c6c5644d0ffb1fc845787d1
delphython <delphython@gmail.com>
2022-04-29 10:33:50 +0300

    Add register_order

--- bakecakeapp/views.py
+++ bakecakeapp/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
+from rest_framework.decorators import api_view
 
 
 def index(request):
-    return render(request, 'bakecakeapp/index.html')
+    return render(request, "bakecakeapp/index.html")
 
 
 def lk(request):
-    return render(request, 'bakecakeapp/lk.html')
+    return render(request, "bakecakeapp/lk.html")
+
+
+@csrf_protect
+@api_view(["POST"])
+def register_order(request):
+    print(request.data)
+    return render(request, "bakecakeapp/payments.html")
