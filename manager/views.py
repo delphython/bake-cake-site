e6d5ec25aeecf91a1485cd07aab7ca7b394b9cfa
delphython <delphython@gmail.com>
2022-05-01 19:39:20 +0300

    Add orders to lk

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Order
+
+
+def view_orders(request):
+    orders = Order.objects.select_related().filter(
+        customer__id=request.user.id
+    )
+
+    return render(
+        request,
+        template_name="bakecakeapp/lk-order.html",
+        context={
+            "orders": orders,
+        },
+    )
