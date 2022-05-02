9175bb787c87b1e47cf64f3af5e3b88a9e3ef173
949027 <82878668+949027@users.noreply.github.com>
2022-05-02 20:55:15 +0700

    Added payment display for order

--- manager/admin.py
+++ manager/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 
 from manager.models import Order, Promocode
+from payment.models import Payment
+
+
+class PaymentInline(admin.StackedInline):
+    model = Payment
+    extra = 0
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    inlines = [PaymentInline]
+    list_display = ('id', 'get_payment', 'status', 'address',
+                    'delivery_date', 'delivery_time')
+
+    @admin.display(ordering='payments__status', description='Payment')
+    def get_payment(self, obj):
+        if obj.payments.first():
+            return obj.payments.first().status
 
 
 @admin.register(Promocode)
